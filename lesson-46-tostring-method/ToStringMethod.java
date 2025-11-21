f00d680b96d83701ca381e40773fd6f4f3e8b85b
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-11-21 15:00:00 -0300

    Update Lesson 46

--- lesson-46-tostring-method/ToStringMethod.java
+++ lesson-46-tostring-method/ToStringMethod.java
@@ -2,6 +2,13 @@ public class ToStringMethod {
 
     public static void main(String[] args){
 
-        // tostring() =
+        // .toString() = method inherited from the Object class
+        //               used to return a string representation of an object
+        //               by default, it returns a hash code as a unique identifier
+        //               it can be overridden to provide meaningful details
+
+        ToStringCar car = new ToStringCar ("Ford", "Mustang", 2025, "Red");
+
+        System.out.println(car);
     }
 }
