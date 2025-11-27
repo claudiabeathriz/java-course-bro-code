b9c15f0c7f163923065108719792a59fad910511
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-11-27 12:09:45 -0300

    Create Lesson 49 - Polymorphism

---
+++ lesson-49-polymorphism/Polymorphism.java
@@ -0,0 +1,21 @@
+public class Polymorphism {
+
+    public static void main(String[] args){
+
+        // polymorphism = "poly" = many; "morph" = shape
+        //                objects can identify as other objects
+        //                objects can be treated as objects of a common superclass
+
+
+        Car car = new Car();
+        Bike bike = new Bike();
+        Boat boat = new Boat();
+
+        Vehicle[] vehicles = {car, bike, boat};
+
+        for(Vehicle vehicle : vehicles){
+            vehicle.go();
+        }
+
+    }
+}
