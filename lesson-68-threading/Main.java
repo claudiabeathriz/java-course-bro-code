6d30ec4ff44d20f03fddd9062c543eadd409f274
Claudia Beathriz O. Pacheco <claudiabopacheco@gmail.com>
2025-12-25 11:23:00 -0300

    Add MyRunnable class to Lesson 68

--- lesson-68-threading/Main.java
+++ lesson-68-threading/Main.java
@@ -6,23 +6,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.setDaemon(true);
+        thread.start();
+
         System.out.println("You have 5 seconds to enter your name");
-
-        for(int i = 1; i <= 5; i++){
-
-            try {
-                Thread.sleep(1000);
-            }
-
-            catch(InterruptedException e){
-                System.out.println("Thread was interrupted");
-            }
-
-            if(i == 5){
-                System.out.println("Time is up!");
-            }
-        }
-
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         System.out.println("Hello, " + name);
